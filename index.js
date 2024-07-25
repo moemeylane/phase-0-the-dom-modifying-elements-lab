e0679ac06ac15856b6f26e2870b5577293ca8dbf
moemeylane <wangarimaureen668@gmail.com>
2024-07-25 09:12:57 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Your Name is the champion'; 
+
+document.body.appendChild(newHeader);
